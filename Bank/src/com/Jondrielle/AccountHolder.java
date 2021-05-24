88709b4d89d494ee7ed9f24eb7c99d45b86d39bc
Jondrielle <jmwilso4@uno.edu>
2021-05-23 23:22:17 -0500

    Added two methods one for withdrawing money and the other for depositing money.

--- Bank/src/com/Jondrielle/AccountHolder.java
+++ Bank/src/com/Jondrielle/AccountHolder.java
@@ -6,16 +6,28 @@ public class AccountHolder {
     private HashMap<String,String> accountHolders = new HashMap<>();
     private String holderUser;
     private String holderPassword;
+    private long balance;
 
-    public AccountHolder(String holderUser, String holderPassword) {
-        System.out.println("Inside constructor");
+    public AccountHolder(String holderUser, String holderPassword, long balance) {
         this.holderUser = holderUser;
         this.holderPassword = holderPassword;
         addUserToAccountHolder(holderUser,holderPassword);
+        this.balance = balance;
     }
 
+    public AccountHolder(String holderUser, String holderPassword) {
+        this.holderUser = holderUser;
+        this.holderPassword = holderPassword;
+        addUserToAccountHolder(holderUser,holderPassword);
+        this.balance = 0;
+    }
+
+    /**
+     * This method checks whether or not a user is in the system already.
+     * @param userName - username login for user
+     * @param password - password login for user
+     */
     public void addUserToAccountHolder(String userName, String password){
-        System.out.println("Inside addUser");
         if(accountHolders.containsKey(userName)){
             System.out.println("Thanks for coming back to The Bank of Jondrielle");
         }
@@ -23,6 +35,24 @@ public class AccountHolder {
             accountHolders.put(userName,password);
             System.out.println("Thanks for joining The Bank of Jondrielle");
         }
+    }
+
+    /**
+     * This method returns how much the user is depositing within their account
+     * @param amountDeposit - This is the amount the user is depositing within their account
+     * @return amount being deposited to the balance
+     */
+    public long deposit(long amountDeposit){
+        return this.balance + amountDeposit;
+    }
+
+    /**
+     * This method withdraws money from the users account
+     * @param amountWithdrawal - This is the amount the user is going to withdraw from their account
+     * @return amount being withdrawn from the account balance
+     */
+    public long withdrawal(long amountWithdrawal){
+        return this.balance - amountWithdrawal;
     }
 
     public String getHolderUser() {
@@ -43,5 +73,12 @@ public class AccountHolder {
     public void setHolderPassword(String holderPassword) {
         System.out.println("Password was reset.");
         this.holderPassword = holderPassword;
+    }
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
     }
 }
